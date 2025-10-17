3bd1141fe3c2f7b0f4d93f2280800e093cd28b8d
nhanchan <nhanchan@LIN-5CG1340X4T.corp.capgemini.com>
2025-10-17 23:04:16 +0530

    test

---
+++ Selenium/src/main/java/Selenium/Selenium/popup.java
@@ -0,0 +1,13 @@
+package Selenium.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class popup {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("");
+			
+	}
+}
